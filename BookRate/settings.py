6966935fc73c7d6979a44f5ff3601d71ab3e258c
Markéta Minářová <minarma5@fit.cvut.cz>
2021-04-07 20:22:28 +0200

    Hardworking session 7.4.2021, web app startup

--- BookRate/settings.py
+++ BookRate/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'webApp'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# config/settings.py
+LOGIN_REDIRECT_URL = '/'
