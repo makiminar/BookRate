d4e19ab4a6a4d07e062ddfa0034d828d4d27a74c
Markéta Minářová <minarma5@fit.cvut.cz>
2021-04-14 18:52:30 +0200

    Django template language

--- webApp/admin.py
+++ webApp/admin.py
@@ -5,3 +5,14 @@ from .models import Book, User, Rating
 admin.site.register(Book)
 admin.site.register(User)
 admin.site.register(Rating)
+
+"""class UserProfile(models.Model):
+    user = models.OneToOneField(User,  on_delete=models.CASCADE)
+
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+
+post_save.connect(create_profile, sender=User) """
--- webApp/views.py
+++ webApp/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.views import generic
 from django.urls import reverse_lazy
+from django.shortcuts import render
 
 
 class SignUpView(generic.CreateView):
