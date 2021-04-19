8ea50a1d713352dc3f5cbcfdc4fe335188ea2cc3
krestrad <krestrad@cvut.cz>
2021-04-19 17:41:56 +0200

    test

--- webApp/views.py
+++ webApp/views.py
@@ -2,9 +2,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views import generic
 from django.urls import reverse_lazy
 from django.shortcuts import render
-
+from webApp.models import Book
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+def recommend(request):
+    bookList = Book.objects.all()
+    context = {'bookList': bookList}
+    return render(request, 'recommend.html', context)
